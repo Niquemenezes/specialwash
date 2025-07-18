f7147a4aee8f9fb37f11cfe8b2c70e1a5001f260
Monique Menezes <122538719+Niquemenezes@users.noreply.github.com>
2025-07-18 15:18:05 +0000

    Corrijo build final y genero dist correctamente

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,20 +1,23 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/front/js/index.js',
+
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'), // Asegúrate que sea 'dist'
+    path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
   },
+
   mode: 'production',
+
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/, // Incluye jsx
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
@@ -25,11 +28,16 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
         type: 'asset/resource',
       },
     ],
   },
+
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+
   plugins: [
     new Dotenv({
       path: './.env',
@@ -44,8 +52,6 @@ module.exports = {
       'process.env.NODE_ENV': JSON.stringify('production'),
     }),
   ],
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
+
   devtool: 'source-map',
 };
